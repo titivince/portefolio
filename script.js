bb010430c065ed8b634eb27299497090b0cb67b8
titivince <72084738+titivince@users.noreply.github.com>
2020-12-06 17:45:56 +0100

    add more css

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 window.onload = function() {
 var i = 0;
+var nav = document.getElementById("nav");
 var nav1 = document.getElementById("nav1");
 var presantation = document.getElementById("pres");
 var type = document.getElementById("name");
 var name = "Vincent Perier";
 
 function topPage(){
-    ;
+    scrollTo(0, 0);
 }
 function block1(){
     presantation.scrollIntoView(true);
@@ -15,11 +16,12 @@ function printName(){
     if(i < name.length){
         type.innerHTML += name.charAt(i);
         i++;
-        setTimeout(printName, 240)
+        setTimeout(printName, 240);
     }
 }
 /* code start */
-nav1.addEventListener("click", block1)
+nav.addEventListener("click", topPage);
+nav1.addEventListener("click", block1);
 setTimeout(printName, 240);
 
 }
