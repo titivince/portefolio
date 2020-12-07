37de8266ccfd7eb920352012b93e83ac751a80c8
titivince <72084738+titivince@users.noreply.github.com>
2020-12-07 17:03:46 +0100

    add block2
    more style
    add img

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ var i = 0;
 var nav = document.getElementById("nav");
 var nav1 = document.getElementById("nav1");
 var presantation = document.getElementById("pres");
+var skill = document.getElementById("skills")
 var type = document.getElementById("name");
 var name = "Vincent Perier";
 
@@ -12,7 +13,10 @@ function topPage(){
 function block1(){
     presantation.scrollIntoView(true);
 }
-function printName(){
+function block2(){
+    skill.scrollIntoView(true)
+}
+function printName(){ /* create the typing effect */
     if(i < name.length){
         type.innerHTML += name.charAt(i);
         i++;
@@ -22,5 +26,6 @@ function printName(){
 /* code start */
 nav.addEventListener("click", topPage);
 nav1.addEventListener("click", block1);
+nav2.addEventListener("click", block2);
 printName();
 }
