33eebb24fecd0e49dfc1900549238c9abbdf33ac
titivince <72084738+titivince@users.noreply.github.com>
2020-12-08 15:52:14 +0100

    add style
    add last js function
    fix css img

--- script.js
+++ script.js
@@ -2,20 +2,28 @@ window.onload = function() {
 var i = 0;
 var nav = document.getElementById("nav");
 var nav1 = document.getElementById("nav1");
-var presantation = document.getElementById("pres");
-var suivant = document.getElementById("suivant");
-var skill = document.getElementById("skills")
+var nav2 = document.getElementById("nav2");
+var nav3 = document.getElementById("nav3");
 var type = document.getElementById("name");
 var name = "Vincent Perier";
+var presantation = document.getElementById("pres");
+var suivant = document.getElementById("suivant");
+var skills = document.getElementById("skills")
+var suivant1 = document.getElementById("suivant1");
+var projets = document.getElementById("projets");
+
 
 function topPage(){
     window.scrollTo({top: 0, behavior: 'smooth'});
 }
 function block1(){
-    window.scrollTo({top: 0, behavior: 'smooth'});
+    presantation.scrollIntoView({behavior: 'smooth'});
 }
 function block2(){
-    window.scrollTo({top: 650, behavior: 'smooth'});
+    skills.scrollIntoView({behavior: 'smooth'});
+}
+function block3(){
+    projets.scrollIntoView({behavior: 'smooth'});
 }
 function printName(){ /* create the typing effect */
     if(i < name.length){
@@ -25,9 +33,12 @@ function printName(){ /* create the typing effect */
     }
 }
 /* code start */
+printName();
 nav.addEventListener("click", topPage);
 nav1.addEventListener("click", block1);
 nav2.addEventListener("click", block2);
 suivant.addEventListener("click", block2);
-printName();
+nav3.addEventListener("click", block3);
+suivant1.addEventListener("click", block3);
+
 }
